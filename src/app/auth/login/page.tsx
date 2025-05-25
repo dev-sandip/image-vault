8e811a618f3ebdee51e891d6eaa0920b750562df
Sandip Sapkota <sandipsapkota9844@gmail.com>
2025-05-25 14:15:21 +0545

    fix error

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -126,7 +126,7 @@ export default function LoginPage() {
               </Link>
             </div>
             <div className="text-center text-sm text-muted-foreground">
-              Don't have an account?{" "}
+              Don&apos;t have an account?{" "}
               <Link
                 href="/auth/register"
                 className="text-primary hover:underline"
